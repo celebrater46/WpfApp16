451eec908ef5f67ce4b1fc4b744e217e4401148d
hideru <jota.16.stigma.world@gmail.com>
2022-01-06 15:30:15 +0900

    added AddBtClick()

--- WpfApp16/MainWindow.xaml.cs
+++ WpfApp16/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Input;
 
 namespace WpfApp16
@@ -34,6 +35,38 @@ namespace WpfApp16
         private void ErrMsg(string msg)
         {
             MessageBox.Show(msg, Properties.Resources.AppTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void AddBtClick(object sender, RoutedEventArgs e)
+        {
+            DateTime d;
+
+            if (System.Text.RegularExpressions.Regex.IsMatch(timeText.Text, "^[0-9]{2}:[0-9]{2}$") &&
+                DateTime.TryParse("2000/01/01 " + timeText.Text, out d))
+            {
+                // whether the inputted text exists
+                if (string.IsNullOrWhiteSpace(msgText.Text))
+                {
+                    // No message
+                    ErrMsg("Type a message.");
+                    msgText.Focus();
+                }
+                else
+                {
+                    // The time and the message both exist
+                    listBox.Items.Add(timeText.Text + "\t" + msgText.Text);
+                    
+                    timeText.Clear();
+                    msgText.Clear();
+                    timeText.Focus();
+                }
+            }
+            else
+            {
+                // The message is not correct as time
+                ErrMsg("Type a date time correctly.");
+                timeText.Focus();
+            }
         }
     }
 }
