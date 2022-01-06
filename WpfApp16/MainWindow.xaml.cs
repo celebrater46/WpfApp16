9133ddc4a8c70482a4c3fe94e751d33e016ffaf1
hideru <jota.16.stigma.world@gmail.com>
2022-01-06 13:36:21 +0900

    Added settings

--- WpfApp16/MainWindow.xaml.cs
+++ WpfApp16/MainWindow.xaml.cs
@@ -11,5 +11,18 @@ namespace WpfApp16
         {
             InitializeComponent();
         }
+
+        private bool IsExistsSaveConfigPath()
+        {
+            if (string.IsNullOrWhiteSpace(Properties.Settings.Default.SavingConfigPath) ||
+                System.IO.File.Exists(Properties.Settings.Default.SavingConfigPath) == false)
+            {
+                return false; // No file or no setting
+            }
+            else
+            {
+                return true; // The file and setting exist 
+            }
+        }
     }
 }
